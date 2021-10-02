205abf215149372aa1098cff6c41e45c2f92d9c6
A'Darius Ellis <85079903+gadaell@users.noreply.github.com>
2021-10-02 13:07:25 -0500

    setup index js

--- models/Tag.js
+++ models/Tag.js
@@ -1,28 +1,28 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
-{
-        id:{
-        type: DataTypes.Integer
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement:true,
+  {
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
-        tag_name: {
-        type: DataTypes.STRING,
-  },
-},
-{
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "tag"
-}
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
 );
 
 module.exports = Tag;
